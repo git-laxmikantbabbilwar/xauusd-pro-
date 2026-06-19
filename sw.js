772498e94bea415e4af89b31162ad4296ea17e39
git-laxmikantbabbilwar <laxmikantbabbilwar@gmail.com>
2026-06-19 18:33:11 +0530

    Implement service worker for caching assets

---
+++ sw.js
@@ -0,0 +1,28 @@
+const CACHE_NAME = 'xau-pro-v1';
+const ASSETS = [
+  '/xauusd-pwa/',
+  '/xauusd-pwa/index.html',
+  '/xauusd-pwa/manifest.json'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => res || fetch(e.request))
+  );
+});
